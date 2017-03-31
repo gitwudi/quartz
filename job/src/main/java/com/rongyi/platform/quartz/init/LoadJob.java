04e1d11aaa74f7676392296db35399f203497575
gitwudi <490840770@qq.com>
2017-03-31 22:39:24 +0800

    本地applicaiton启用client

--- job/src/main/java/com/rongyi/platform/quartz/init/LoadJob.java
+++ job/src/main/java/com/rongyi/platform/quartz/init/LoadJob.java
@@ -4,11 +4,9 @@ import java.util.Date;
 
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
-import org.quartz.impl.StdSchedulerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 import com.rongyi.platform.quartz.biz.JobBiz;
 
@@ -16,38 +14,42 @@ public class LoadJob {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-	@Autowired
-	private JobBiz jobBiz;
-	
-	 @Autowired
-	 private SchedulerFactoryBean schedulerFactoryBean;
+	private Scheduler scheduler;
 
 	public void init() throws SchedulerException {
-//		List<QzJob> jobList = new ArrayList<>();
-//		QzJob qzJob = new QzJob();
-//		qzJob.setName("hi");
-//		qzJob.setGroupName("defaut");
-//		qzJob.setTriggerName("hiTrigger");
-//		qzJob.setClassName("com.rongyi.platform.quartz.job.HiJob");
-//		qzJob.setCronExpression("0/5 * * * * ? ");
-//		jobList.add(qzJob);
-//		// 可执行的任务列表
-//		log.info("初始化加载定时任务......");
-//		for (QzJob job : jobList) {
-//			jobBiz.add(job);
-//		}
-		
-		System.setProperty("org.quartz.properties", "config/server.properties");
-		Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-//		Scheduler scheduler = schedulerFactoryBean.getScheduler();
-//		System.out.println(System.getProperty("org.quartz.properties"));
+		// List<QzJob> jobList = new ArrayList<>();
+		// QzJob qzJob = new QzJob();
+		// qzJob.setName("hi");
+		// qzJob.setGroupName("defaut");
+		// qzJob.setTriggerName("hiTrigger");
+		// qzJob.setClassName("com.rongyi.platform.quartz.job.HiJob");
+		// qzJob.setCronExpression("0/5 * * * * ? ");
+		// jobList.add(qzJob);
+		// // 可执行的任务列表
+		// log.info("初始化加载定时任务......");
+		// for (QzJob job : jobList) {
+		// jobBiz.add(job);
+		// }
+
+		// System.setProperty("org.quartz.properties", "config/server.properties");
+		// Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+		// Scheduler scheduler = schedulerFactoryBean.getScheduler();
+		// System.out.println(System.getProperty("org.quartz.properties"));
 		// RMI with Quartz requires a special security manager
-//		if (System.getSecurityManager() == null) {
-//			System.setSecurityManager(new java.rmi.RMISecurityManager());
-//		}
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new java.rmi.RMISecurityManager());
+		}
 		scheduler.start();
 		log.info("Quartz RMI Server started at " + new Date());
 		log.info("RMI Clients may now access it. ");
-
 	}
+
+	public Scheduler getScheduler() {
+		return scheduler;
+	}
+
+	public void setScheduler(Scheduler scheduler) {
+		this.scheduler = scheduler;
+	}
+
 }
--- web/src/main/java/com/rongyi/platform/quartz/client/QuartzRMIClient.java
+++ web/src/main/java/com/rongyi/platform/quartz/client/QuartzRMIClient.java
@@ -4,9 +4,12 @@ import org.quartz.CronScheduleBuilder;
 import org.quartz.CronTrigger;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
+import org.quartz.JobKey;
 import org.quartz.Scheduler;
+import org.quartz.SchedulerFactory;
 import org.quartz.TriggerBuilder;
 import org.quartz.TriggerKey;
+import org.quartz.impl.StdSchedulerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +18,7 @@ import com.rongyi.platform.quartz.module.model.QzJob;
 
 public class QuartzRMIClient {
 
-	@Autowired
-	private Scheduler quartzScheduler;
+	private Scheduler scheduler;
 
 	public void start() throws Exception {
 
@@ -28,6 +30,9 @@ public class QuartzRMIClient {
 		// Get a reference to the remote scheduler
 //		Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
 //		Scheduler scheduler = schedulerFactoryBean.getScheduler();
+		
+//		SchedulerFactory sf = new StdSchedulerFactory();  
+//        Scheduler scheduler = sf.getScheduler(); 
 
 		// Define the job to add
 //		JobDetail job = new JobDetail("remotelyAddedJob", "default", SimpleJob.class);
@@ -43,27 +48,38 @@ public class QuartzRMIClient {
 		qzJob.setTriggerName("hiTrigger");
 		qzJob.setClassName("com.rongyi.platform.quartz.job.HiJob");
 		qzJob.setCronExpression("0/5 * * * * ? ");
-		
-		TriggerKey triggerKey = TriggerKey.triggerKey(qzJob.getName(), qzJob.getGroupName());
+//		
+//		TriggerKey triggerKey = TriggerKey.triggerKey(qzJob.getName(), qzJob.getGroupName());
+//
+//		CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
+//		
+//		Class clazz = Class.forName(qzJob.getClassName());
+//
+//		JobDetail jobDetail = JobBuilder.newJob(clazz).withIdentity(qzJob.getName(), qzJob.getGroupName())
+//				.build();
+//		 jobDetail.getJobDataMap().put("haha", "1");// 启动定时器时传参
+//
+//		CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(qzJob.getCronExpression());
+//		// trigger = TriggerBuilder.newTrigger().withIdentity(qzJob.getName(), qzJob.getGroupName())
+//		// .withSchedule(scheduleBuilder).build();// 启动会立即执行
+//		
+//		trigger = TriggerBuilder.newTrigger().withIdentity(qzJob.getName(), qzJob.getGroupName())
+//				.withSchedule(scheduleBuilder.withMisfireHandlingInstructionDoNothing()).build();// 启动会在下次触发时执行
+//		// schedule the remote job
+//		scheduler.scheduleJob(jobDetail, trigger);
 
-		CronTrigger trigger = (CronTrigger) quartzScheduler.getTrigger(triggerKey);
-		
-		Class clazz = Class.forName(qzJob.getClassName());
-
-		JobDetail jobDetail = JobBuilder.newJob(clazz).withIdentity(qzJob.getName(), qzJob.getGroupName())
-				.build();
-		 jobDetail.getJobDataMap().put("haha", "1");// 启动定时器时传参
-
-		CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(qzJob.getCronExpression());
-		// trigger = TriggerBuilder.newTrigger().withIdentity(qzJob.getName(), qzJob.getGroupName())
-		// .withSchedule(scheduleBuilder).build();// 启动会立即执行
-		
-		trigger = TriggerBuilder.newTrigger().withIdentity(qzJob.getName(), qzJob.getGroupName())
-				.withSchedule(scheduleBuilder.withMisfireHandlingInstructionDoNothing()).build();// 启动会在下次触发时执行
-		// schedule the remote job
-		quartzScheduler.scheduleJob(jobDetail, trigger);
-
+		JobKey jobKey = JobKey.jobKey(qzJob.getName(), qzJob.getGroupName());
+//        scheduler.pauseJob(jobKey);
+        scheduler.resumeJob(jobKey);
 		log.info("Remote job scheduled.");
+	}
+
+	public Scheduler getScheduler() {
+		return scheduler;
+	}
+
+	public void setScheduler(Scheduler scheduler) {
+		this.scheduler = scheduler;
 	}
 
 	public static void main(String[] args) throws Exception {
